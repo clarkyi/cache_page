e5dc592d732c7efe7ebe8d8ffcef78ece3d8472f
clarkyi <yiwenqi520@sina.com>
2015-04-25 16:07:24 +0800

    Update amazon.rb

--- site/amazon.rb
+++ site/amazon.rb
@@ -1,13 +1,26 @@
 #encoding: utf-8
 
-module CachePage
-  module Site
-    module Amazon
-      extend Basic   
-      PRICE_AREA_HTML_TAG_ID = "priceblock_ourprice"
-      TITLE_AREA_HTML_TAG_ID = "div.a-section h1#title span#productTitle"
-      IMAGE_AREA_HTML_TAG_ID = "altImages"
-      
+module CatchPage
+  module Amazon
+    extend Basic
+    PRICE_AREA_HTML_TAG_ID = "span#priceblock_ourprice"
+    TITLE_AREA_HTML_TAG_ID = "div.a-section h1#title span#productTitle"
+    IMAGE_AREA_HTML_TAG_ID = "span.a-button-inner span.a-button-text img"
+    REGULAR_MATCH_IMAGE = /\._.*_\./
+    
+    def self.get_page_info url
+      @doc = html_doc url
+      merge_info url
     end
-  end  
+    private
+    def self.merge_info url
+      { purchase_link: url,
+        title: title(TITLE_AREA_HTML_TAG_ID), 
+        price: price(PRICE_AREA_HTML_TAG_ID), 
+        content: images(IMAGE_AREA_HTML_TAG_ID) do |doc|
+          doc.collect{|response|"<img src='#{get_large_img(response.attributes['src'].value,REGULAR_MATCH_IMAGE)}'/>"}
+        end.join
+      }
+    end
+  end
 end
